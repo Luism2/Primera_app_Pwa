57229789250abd3b89a499488f8353236ec7ce86
Luis Miguel Vazquez Juarez <luisjuares73@gmail.com>
2022-09-22 15:13:48 -0500

    continuacion del 15abo video

--- prom-1.js
+++ prom-1.js
@@ -1,1 +1,13 @@
-console.log('Hola a todos ')
+function sumarUno(numero, callback){
+
+    setTimeout(function(){
+
+    },800);
+
+    //return numero  + 1 ;
+    callback(numero + 1);
+}
+
+sumarUno(5, function(nuevoValor){
+    console.log(nuevoValor)
+});
