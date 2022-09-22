b99f7821aa118cca2bb4440db3aa95b997d679a0
Luis Miguel Vazquez Juarez <luisjuares73@gmail.com>
2022-09-22 15:25:30 -0500

    Terminando video 15

--- prom-1.js
+++ prom-1.js
@@ -1,13 +1,34 @@
 function sumarUno(numero, callback){
-
-    setTimeout(function(){
-
-    },800);
-
-    //return numero  + 1 ;
-    callback(numero + 1);
+if (numero >= 7){
+    callback('Este numero es demaciado alto');
+    return;
 }
+setTimeout(function(){
+    callback(null, numero + 1 );
+}, 800);
+}
+sumarUno(5, function(error, nuevoValor){
 
-sumarUno(5, function(nuevoValor){
-    console.log(nuevoValor)
-});
+    if(error){
+        console.log(error);
+        return;
+    }
+
+    sumarUno(nuevoValor, function(error,nuevoValor2){
+
+        if(error){
+            console.log(error);
+            return;
+        }
+
+        sumarUno(nuevoValor2, function(error,nuevoValor3){
+
+            if(error){
+                console.log(error);
+                return;
+            }
+
+            console.log(nuevoValor3);
+        });
+    });
+  });
