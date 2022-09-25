a89d7ca1dc003769ef209cfb133130af4d2b92ed
Luis Miguel Vazquez Juarez <luisjuares73@gmail.com>
2022-09-25 14:26:04 -0500

    terminaacion del fetch 5

--- fetch-5.js
+++ fetch-5.js
@@ -1,18 +1,17 @@
 
 
-fetch('https://reqres.in/api/users/1')
+fetch('https://reqres.in/api/users/10000')
     .then(resp => {
+        if(resp.ok){
+            return resp-json();
+        }
+        else{
+            throw new Error('No esxiste este usuario')
+        }
+    })
 
-        resp.clone().json()
-            .then(usuario => {
-                console.log(usuario.data);
-            });
-        resp.clone().json().then(usuario => {
-            console.log(usuario.data);
-        });
-
-        resp.json().then(usuario => {
-            console.log(usuario.data);
-        });
-
+    .then(console.log)
+    .catch(error => {
+        console.log('Error en la peticion');
+        console.log(error);
     });
